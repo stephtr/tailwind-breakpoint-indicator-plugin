3876b74eecab230f46207e960d35cdbf21361a7a
Stephan Troyer <stephantroyer@live.at>
2024-04-15 22:45:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Tailwind breakpoint indicator plugin
+
+This Tailwind plugin displays a small screen size indicator:
+![Screenshot with the plugin in use](https://github.com/stephtr/tailwind-breakpoint-indicator-plugin/raw/main/screenshot.png)
+
+## Installation
+
+```bash
+npm install tailwind-breakpoint-indicator-plugin
+```
+
+## Quick Start
+
+Just add the following to your `tailwind.config.js`:
+
+```js
+module.exports = {
+  // ...
+  plugins: [
+    // ...
+    require("tailwind-breakpoint-indicator-plugin"),
+  ],
+};
+```
+
+## Credits
+
+This plugin was inspired by Gist from [Lelectrolux](https://gist.github.com/Lelectrolux/8f9a78491a5c9617078a73c091e01415)
---
+++ index.js
@@ -0,0 +1,35 @@
+export default function TailwindBreakpointIndicatorPlugin({ addBase, theme }) {
+  if (process.env.NODE_ENV === "production") return;
+
+  const screens = theme("screens", {});
+  const breakpoints = Object.keys(screens);
+
+  addBase({
+    "body::after": {
+      content: `"–"`,
+      position: "fixed",
+      left: ".5rem",
+      bottom: ".5rem",
+      width: "2rem",
+      height: "2rem",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "100%",
+      fontSize: ".875rem",
+      fontWeight: "600",
+      zIndex: "99999",
+      background: "light-dark(#edf2f7, #111)",
+      color: "light-dark(#567, #eee)",
+      border: "1px solid light-dark(#9ab, #cde)",
+    },
+    ...breakpoints.reduce((acc, current) => {
+      acc[`@media (min-width: ${screens[current]})`] = {
+        "body::after": {
+          content: `"${current}"`,
+        },
+      };
+      return acc;
+    }, {}),
+  });
+}
